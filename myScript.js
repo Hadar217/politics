ec797d97970f67ebce7677978975b49d0312e76b
Hadar217 <53275213+Hadar217@users.noreply.github.com>
2019-07-24 15:00:14 -0400

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,11 @@
+function republicanCandidates() {
+  document.getElementById("button").innerHTML
+= "Michael Bennet (D), Joe Biden (D), Bill de Blasio (D), Cory Booker (D), Steve Bullock (D), Pete Buttigieg (D), Julián Castro (D), John Delaney (D), Tulsi Gabbard (D), Kirsten Gillibrand (D), Mike Gravel (D), Kamala Harris (D), John Hickenlooper (D), Jay Inslee (D), Amy Klobuchar (D), Wayne Messam (D), Seth Moulton (D), Beto O'Rourke (D), Tim Ryan (D), Bernie Sanders (I), Joe Sestak (D), Tom Steyer (D), President Donald Trump (R), Elizabeth Warren (D), Bill Weld (R), Marianne Williamson (D), Andrew Yang (D)";
+}
+
+
+function mapQ() {
+  // document.getElementById("map").src
+  // = <img id="mapQ" src="QueensMap.png" style="width:100px">;
+  location.href = "pol.html";
+}
